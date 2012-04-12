75f66349ac20bbc3fca60a14dd517290f4717e25
Reed Law <reed@smashingboxes.com>
2012-04-12 09:26:40 -0400

    allow setting password digest for user import

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   attr_accessible :admin, as: :admin
-  attr_accessible :email, :password, :password_confirmation, as: [ :default, :admin ]
+  attr_accessible :email, :password_digest, :password, :password_confirmation, as: [ :default, :admin ]
   
   validates_uniqueness_of :email
 end
