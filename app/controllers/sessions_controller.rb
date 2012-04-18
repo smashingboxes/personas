b270c9b07c8917fd5efb87699f01636caafeecd4
Reed Law <reed@smashingboxes.com>
2012-04-18 13:29:24 -0400

    show error on login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,32 @@ class SessionsController < ApplicationController
   include UrlHelper
 
   def new
+    render_login
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      session['ref'] = nil
+      redirect_to return_url
+    else
+      render_login("Email or password is invalid.")
+    end
+  end
+
+  def destroy
+    # Remove user's session and return url
+    session[:user_id] = session[:return_url] = nil
+    if params[:redirect] 
+      redirect_to params[:redirect]  
+    else
+      redirect_to root_url, :notice => "Logged out!"
+    end
+  end
+
+  private
+  def render_login(error = nil)
     # Check if user is coming from another site
     ref = make_absolute(request.env['HTTP_REFERER'])
     host = make_absolute(request.url)
@@ -14,7 +40,12 @@ class SessionsController < ApplicationController
       end
       # Get the client app's login page and alter it,
       # adding authenticity token and rewritting urls.
-      doc = Nokogiri::HTML(open("#{ref}/login"))
+      if error
+        url = "#{ref}/login?error=#{URI.escape(error)}"
+      else
+        url = "#{ref}/login"
+      end
+      doc = Nokogiri::HTML(open(url))
       if token = doc.at_css("input[name=authenticity_token]")
         token["value"] = form_authenticity_token
       end
@@ -23,28 +54,6 @@ class SessionsController < ApplicationController
       doc.css("link").map {|l|rewrite(l, 'href', ref)}
       doc.css("script").map {|s|rewrite(s, 'src', ref)}
       render :text => doc.to_s
-    end
-  end
-
-  def create
-    user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      session['ref'] = nil
-      redirect_to return_url
-    else
-      flash.now.alert = "Email or password is invalid"
-      redirect_to login_path
-    end
-  end
-
-  def destroy
-    # Remove user's session and return url
-    session[:user_id] = session[:return_url] = nil
-    if params[:redirect] 
-      redirect_to params[:redirect]  
-    else
-      redirect_to root_url, :notice => "Logged out!"
     end
   end
 end
